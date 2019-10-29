035177989dbf5f12a3c41f120d68afc5a30dec8a
fakhry <fakhryafa@gmail.com>
2019-10-29 16:16:24 +0700

    menu activity

--- app/src/main/java/Project/PBM/LoginActivity.java
+++ app/src/main/java/Project/PBM/LoginActivity.java
@@ -1,7 +1,9 @@
 package Project.PBM;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void pindah (View view){
+        Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+        startActivity(intent);
+    }
+    public void masuk (View view){
+        Intent intent = new Intent(LoginActivity.this,MenuActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/Project/PBM/SplashScreenActivity.java
+++ app/src/main/java/Project/PBM/SplashScreenActivity.java
@@ -15,8 +15,8 @@ public class SplashScreenActivity extends AppCompatActivity {
             @Override
             public void run() {
                 startActivity(new Intent(SplashScreenActivity.this,LoginActivity.class));
+                finish();
             }
         },2000);
-
     }
 }
